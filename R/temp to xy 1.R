ce5f7ef918db0f3481f1bce2d73fcccacd6ea5cb
AkselA <akselahenriksen@gmail.com>
2018-03-11 23:00:18 +0100

    wrapped splinefuns in generator function

--- R/temp to xy 1.R
+++ R/temp to xy 1.R
@@ -1,11 +1,12 @@
 
-suff <- expand.grid(c("x", "y", "P"), c("2", "10"), stringsAsFactors=FALSE)
-K2 <- list()
+fungen <- function() {
+    suff <- expand.grid(c("x", "y", "P"), c("2", "10"), stringsAsFactors=FALSE)
+    K2 <- list()
 
-for (i in 1:nrow(suff)) {
-	K2[[i]] <- splinefun(kelvin[[suff[i, 2]]][c("K", suff[i, 1])])
+    for (i in 1:nrow(suff)) {
+    	K2[[i]] <- splinefun(kelvin[[suff[i, 2]]][c("K", suff[i, 1])])
+    }
+
+    names(K2) <- apply(suff, 1, paste, collapse="")
+    K2
 }
-
-names(K2) <- apply(suff, 1, paste, collapse="")
-rm(suff)
-
