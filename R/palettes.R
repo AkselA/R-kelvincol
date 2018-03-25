75fc8086db7fd527a360c74b2b13c3713824d763
AkselA <akselahenriksen@gmail.com>
2018-03-25 21:34:20 +0200

    export palettes

--- R/palettes.R
+++ R/palettes.R
@@ -1,36 +1,36 @@
 
-temp.colors.fun <- function() {	
-    temp <- colorRampPalette({
-      k <- seq(900, 14000, length.out=100)
-      r <- K2RGB(k, Y=splval(k))
-      h <- rgb2hsv(t(r), maxColorValue=1)
-      h[2,] <- h[2,] * 1.5
-      h[2, h[2,] > 1] <- 1
-      h <- hsv(h[1,], h[2,], h[3,])
-      h
-      })
+# # temp.colors.fun <- function() {	
+    # temp <- colorRampPalette({
+      # k <- seq(900, 14000, length.out=100)
+      # r <- K2RGB(k, Y=splval(k))
+      # h <- rgb2hsv(t(r), maxColorValue=1)
+      # h[2,] <- h[2,] * 1.5
+      # h[2, h[2,] > 1] <- 1
+      # h <- hsv(h[1,], h[2,], h[3,])
+      # h
+      # })
     
-      # image2(matrix(1:(16*16), 16), col=temp.colors(16*16))
+      # # image2(matrix(1:(16*16), 16), col=temp.colors(16*16))
     
-    templog <- colorRampPalette({
-      k <- seq(log(1000), log(45000), length.out=100)
-      k <- exp(k)
-      r <- K2RGB(k, Y=splval(k), method="kim")
-      h <- rgb2hsv(t(r), maxColorValue=1)
-      h[2,] <- h[2,] * 1.5
-      h[2, h[2,] > 1] <- 1
-      h <- hsv(h[1,], h[2,], h[3,])
-      h  
-      })
+    # templog <- colorRampPalette({
+      # k <- seq(log(1000), log(45000), length.out=100)
+      # k <- exp(k)
+      # r <- K2RGB(k, Y=splval(k), method="kim")
+      # h <- rgb2hsv(t(r), maxColorValue=1)
+      # h[2,] <- h[2,] * 1.5
+      # h[2, h[2,] > 1] <- 1
+      # h <- hsv(h[1,], h[2,], h[3,])
+      # h  
+      # })
     
-    list(temp, templog)
-}
+    # list(temp, templog)
+# }
 
 #' @export
-kelvin.colors <- function(n) kelvincol::temp.colors.fun()[[1]](n)
+kelvin.colors <- function(n) kelvincol::temp.colors[[1]](n)
 
 #' @export
-kelvinlog.colors <- function(n) kelvincol::temp.colors.fun()[[1]](n)
+kelvinlog.colors <- function(n) kelvincol::temp.colors[[1]](n)
 
 # image2(matrix(1:(16*16), 16), col=templog.colors(16*16))
 # plot(1:16, bg=temp.colors(16), pch=21, cex=6)
